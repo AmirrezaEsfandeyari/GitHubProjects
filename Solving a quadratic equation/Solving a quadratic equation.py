7cabbb558ad90120c9a84d2b3af2d5b5356cac6f
amir <amir70108reza@gmail.com>
2026-08-03 21:07:20 +0330

    Quadratic Equation Solver Program editing

--- Solving a quadratic equation/Solving a quadratic equation.py
+++ Solving a quadratic equation/Solving a quadratic equation.py
@@ -1,4 +1,4 @@
-print('a program to solve quadratic equation.\nenter coefficients of the equation as follows:\naX^2+bX+c')
+print('a program to solve quadratic equation.\nenter coefficients of the equation as follows:\nax²+bx+c')
 a=int(input('please enter coefficient "a":'))
 b=int(input('please enter coefficient "b":'))
 c=int(input('please enter coefficient "c":'))
@@ -7,9 +7,9 @@ Delta=((b**2)-(4*(a*c)))**0.5
 if Delta>1:
     x1=(-b+Delta)/(2*a)
     x2 = (-b-Delta)/(2*a)
-    print(f'the equation has two solutions.X1={x1} and X2={x2}')
+    print(f'the equation has two solutions.x={x1} and x={x2}')
 elif Delta==0:
     x=-b/(2*a)
-    print(f'the equation has one solutions.X={x}')
+    print(f'the equation has one solutions.x={x}')
 else:
     print('the equation has no answer..')
