3b126c417355edf118bf4a8e6da387fd8cfec31a
amir <amir70108reza@gmail.com>
2026-08-03 21:18:59 +0330

    Quadratic Equation Solver Program edited.

--- Solving a quadratic equation/Solving a quadratic equation.py
+++ Solving a quadratic equation/Solving a quadratic equation.py
@@ -3,13 +3,13 @@ a=int(input('please enter coefficient "a":'))
 b=int(input('please enter coefficient "b":'))
 c=int(input('please enter coefficient "c":'))
 
-Delta=((b**2)-(4*(a*c)))**0.5
-if Delta>1:
-    x1=(-b+Delta)/(2*a)
-    x2 = (-b-Delta)/(2*a)
-    print(f'the equation has two solutions.x={x1} and x={x2}')
-elif Delta==0:
-    x=-b/(2*a)
-    print(f'the equation has one solutions.x={x}')
+Delta=((b ** 2)-(4 * (a * c)))
+if Delta > 1:
+    x1=(-b + Delta ** 0.5)/(2 * a)
+    x2 = (-b - Delta ** 0.5)/(2 * a)
+    print(f'the equation has two solutions.x = {x1} and x = {x2}')
+elif Delta == 0:
+    x= -b / (2 * a)
+    print(f'the equation has one solutions.x = {x}')
 else:
     print('the equation has no answer..')
